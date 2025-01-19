f7392f31a326a489775d028f57710ee80236405e
BigBearBrax <52250606+BraxtonAndrew@users.noreply.github.com>
2025-01-19 13:15:47 -0700

    Update Change Guest to Member.ps1

--- PowerShell Scripts/Change Guest to Member.ps1
+++ PowerShell Scripts/Change Guest to Member.ps1
@@ -1,3 +1,13 @@
-Connect-MgGraph
+# Install Graph module if not already installed
+Install-Module -Name Microsoft.Graph -Scope CurrentUser
 
+# Connected to Graph with appropriate permissions
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# Open Entra and search for the user
+# Click on the user and go to the overview tab
+# Copy their UserID (Located next to their UPN/Email Address)
+
+# Update the user account
 Update-MgUser -UserType "Member"
+# Paste in the UserID in the terminal when it asks for it
